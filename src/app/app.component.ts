6e22bd80bdc7180227a4f3a25c55386ccf61cf63
daniikprog <oh_my_glob@inbox.ru>
2024-07-06 16:29:11 +0900

    fetching data from backend with 1 endpoint "/products"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import {products as data}   from './data/products';
+import { ProductsService } from './services/products.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Learning angular for review';
-  products: IProduct[]=data;
+  products: IProduct[] = [];
+
+  constructor(private productsService: ProductsService) {}
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => {
+      this.products= products;
+    });
+  }
 }
